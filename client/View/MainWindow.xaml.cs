b9c7c05b83dedd6bff8d707d4448099efe843e48
NekichNet <102032304+NekichNet@users.noreply.github.com>
2025-12-11 22:19:21 +0500

    Добавьте файлы проекта.

---
+++ client/View/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using client.ViewModel;
+using MaterialIcon = MahApps.Metro.IconPacks.PackIconMaterial;
+using MaterialKind = MahApps.Metro.IconPacks.PackIconMaterialKind;
+
+namespace client.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel(this);
+            ThemeButton.Content = (DataContext as MainWindowViewModel).ChangeTheme();
+        }
+
+        private void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ThemeButton.Content = (DataContext as MainWindowViewModel).ChangeTheme();
+        }
+
+        private void HeaderBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) Application.Current.MainWindow.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                MaxButton.Content = new MaterialIcon() { Kind = MaterialKind.WindowMaximize };
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                MaxButton.Content = new MaterialIcon() { Kind = MaterialKind.WindowRestore };
+            }
+
+        }
+    }
+}
---
+++ client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using client.View;
+using MaterialIcon = MahApps.Metro.IconPacks.PackIconMaterial;
+using MaterialKind = MahApps.Metro.IconPacks.PackIconMaterialKind;
+
+namespace client.ViewModel
+{
+    public class MainWindowViewModel 
+    {
+        public MainWindow View { get; set; }
+
+        private List<Tuple<string, MaterialKind>> _themes = new List<Tuple<string, MaterialKind>> {
+            new Tuple<string, MaterialKind>("Dark", MaterialKind.MoonWaxingCrescent),
+            new Tuple<string, MaterialKind>("Light", MaterialKind.LightbulbOn)
+        };
+        private int _themeIndex = -1;
+
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+            View = mainWindow;
+        }
+
+        public MaterialIcon ChangeTheme()
+        {
+            _themeIndex = _themes.Count - 1 == _themeIndex ? 0 : _themeIndex + 1;
+            var baseUri = new Uri("View/Themes/Base.xaml", UriKind.RelativeOrAbsolute);
+            var uri = new Uri("View/Themes/" + _themes[_themeIndex].Item1 + ".xaml", UriKind.RelativeOrAbsolute);
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(Application.LoadComponent(baseUri) as ResourceDictionary);
+            Application.Current.Resources.MergedDictionaries.Add(Application.LoadComponent(uri) as ResourceDictionary);
+            return new MaterialIcon() { Kind = _themes[_themes.Count - 1 == _themeIndex ? 0 : _themeIndex + 1].Item2 };
+        }
+    }
+}
